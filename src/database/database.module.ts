ef799407ac76b877654d2b6b3d0cbfa1845bf7c1
leducdung <leducdung149@gmail.com>
2021-03-22 20:40:41 +0700

    d

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,5 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 export const databaseModule  = MongooseModule.forRootAsync({
   useFactory: () => ({
     uri: 'mongodb+srv://leducdung:1@uploads.epnvd.mongodb.net/homemarket?retryWrites=true&w=majority',
+    useNewUrlParser: true,
+    useCreateIndex: true,
   }),
 });
