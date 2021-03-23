c99a2312f493d9973c0768fb84156029994a7dcc
leducdung <leducdung149@gmail.com>
2021-03-23 09:51:11 +0700

    d

--- src/main.ts
+++ src/main.ts
@@ -7,29 +7,26 @@ import { StoreOwnersModule } from './domains/storeOwners/storeOwners.module'
 import { EmployeesModule } from './domains/employees/employees.module';
 import { ProductsModule } from './domains/\bproducts/products.module';
 
-async function bootstrap() {
-  async function bootstrap(){
-    const app = await NestFactory.create(MyAppModule);
+async function bootstrap(){
+  const app = await NestFactory.create(MyAppModule);
 
-    const options =new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('HomeMarket')
-    .build();
+  const options =new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('HomeMarket')
+  .build();
 
-    const apiDocument = SwaggerModule.createDocument(app, options,{
-        include:[TestModule,
-          UsersModule,
-          StoreOwnersModule,
-          EmployeesModule,
-          ProductsModule,
-        ],
-    });
-    SwaggerModule.setup('api', app, apiDocument);
-      await app.listen(process.env.PORT, '0.0.0.0');
-    }
-    bootstrap();
+  const apiDocument = SwaggerModule.createDocument(app, options,{
+      include:[TestModule,
+        UsersModule,
+        StoreOwnersModule,
+        EmployeesModule,
+        ProductsModule,
+      ],
+  });
+  SwaggerModule.setup('api', app, apiDocument);
+    await app.listen(process.env.PORT, '0.0.0.0');
 }
 
 bootstrap();
