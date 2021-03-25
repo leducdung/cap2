c9fe4fb179e3193f87578d6e67057cabb0c250a8
leducdung <leducdung149@gmail.com>
2021-03-25 21:24:54 +0700

    d

--- src/main.ts
+++ src/main.ts
@@ -35,10 +35,11 @@ async function bootstrap() {
 
   app.enableCors()
 
-  const PORT = 3000
-  await app.listen( PORT || 3000 , '0.0.0.0' , function () {
-    console.log('Server started.......');
-  });
+  var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+  var server_host = process.env.YOUR_HOST || '0.0.0.0';
+  await app.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+  })
 }
 
 bootstrap();
